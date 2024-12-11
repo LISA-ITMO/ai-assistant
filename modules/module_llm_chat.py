1b3be220da1c6699da0bc44a72a9f242ab10f862
RomanKharkovskoy <roman.kharkovskoy@yandex.ru>
2024-12-11 20:15:42 +0300

    llm 0.1

---
+++ modules/module_llm_chat.py
@@ -0,0 +1,49 @@
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_huggingface import HuggingFacePipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+
+
+def initialize_llm(model_name="meta-llama/Llama-3.2-1B-Instruct"):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+
+    hf_pipeline = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_length=4096,
+        temperature=0.2,
+        top_p=1,
+    )
+
+    llm = HuggingFacePipeline(
+        pipeline=hf_pipeline,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+    )
+
+    return llm
+
+
+def generate_response(llm, context, question):
+    template = '''
+        Контекст: {context}
+        Исходя из контекста, ответьте на следующий вопрос:
+        Вопрос: {question}
+        Предоставьте ответ только на основе предоставленного контекста, без использования общих знаний. 
+        Ответ должен быть непосредственно взят из предоставленного контекста.
+        Пожалуйста, исправьте грамматические ошибки для улучшения читаемости.
+        Если в контексте нет информации, достаточной для ответа на вопрос, укажите, что ответ отсутствует в данном контексте.
+        Пожалуйста, включите источник информации в качестве ссылки, поясняющей, как вы пришли к своему ответу.
+    '''
+
+    prompt = PromptTemplate(
+        input_variables=["context", "question"], template=template)
+    formatted_prompt = prompt.format(context=context, question=question)
+
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    response = llm_chain.run(formatted_prompt)
+
+    return response
