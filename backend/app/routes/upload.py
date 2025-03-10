5e7cf2af897741e6efbe5a688c52e2235e587b64
RomanKharkovskoy <roman.kharkovskoy@yandex.ru>
2025-03-10 11:32:04 +0300

    Remove upload file route from backend

--- backend/app/routes/upload.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-
-router = APIRouter()
-
-
-@router.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    content = await file.read()
-    return {"filename": file.filename, "size": len(content)}
